026c6fd2c3f7271ed04a211e37e63c483df72c3b
Mattias <mattiasj@protonmail.com>
2020-02-17 13:20:10 +0100

    Cards funkar

--- Cards/src/app/app.component.ts
+++ Cards/src/app/app.component.ts
@@ -10,19 +10,19 @@ export class AppComponent {
     { 
       title: 'Neat tree',
       imageUrl: 'assets/tree.jpeg',
-      username: '@nature',
+      username: 'nature',
       content: 'I saw this neat tree today.' 
     },    
     { 
       title: 'Snowy mountain',
       imageUrl: 'assets/mountain.jpeg',
-      username: '@mountain12',
+      username: 'mountain12',
       content: 'Here is a picture of a snowy mountain.' 
     },    
     { 
       title: 'Mountain biking',
       imageUrl: 'assets/biking.jpeg',
-      username: '@biking45',
+      username: 'biking45',
       content: 'The view when I went biking.' 
     }
   ]
