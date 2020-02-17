3a2e6e7b356cfc8f749922184f15d553b8529b73
Unknown <mattiasj@protonmail.com>
2020-02-17 18:59:53 +0100

    Ny typechallenge

---
+++ typingChallenge/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  randomText:string = lorem.sentence();
+
+  onInput(value: string){
+    console.log(value);
+  }
+
+}
