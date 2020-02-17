9009548e02e30f22c36c63219ef7924df7d8743c
Unknown <mattiasj@protonmail.com>
2020-02-17 21:17:15 +0100

    Fungerande typechallenge

--- typingChallenge/src/app/app.component.ts
+++ typingChallenge/src/app/app.component.ts
@@ -8,10 +8,36 @@ import { lorem } from 'faker';
 })
 export class AppComponent {
 
-  randomText:string = lorem.sentence();
+  randomText: string = lorem.sentence();
+  inputText: string = '';
+  success: boolean = false;
 
   onInput(value: string){
-    console.log(value);
+
+    this.inputText = value;
+    this.isSuccess(this.inputText);
+  }
+
+  validateInput(original: string, input: string){
+
+    if(!input){
+      return 'pending';
+    }
+
+    return original === input ? 'correct' : 'incorrect';
+  }
+
+
+  isSuccess(input: string){
+
+    if(this.randomText === input){
+      this.success = true;
+    }
+    else{
+      this.success = false;
+    }
+
+    this.inputText = input;
   }
 
 }
